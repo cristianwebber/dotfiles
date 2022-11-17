5a4892003059290257fdbb44cc7a8a7c6d575dda
Cristian Webber <cbwebbers@gmail.com>
2022-11-17 14:56:23 -0300

    feat: rm extra space

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -28,7 +28,7 @@ key_mapper('n', "<C-e>", ":NvimTreeFindFile<CR>")
 key_mapper('i', "<C-c>", "<Esc>")
 
 -- Telescope
-key_mapper('n', "<leader>ff", " <cmd>Telescope find_files<cr>")
-key_mapper('n', "<leader>fg", " <cmd>Telescope live_grep<cr>")
+key_mapper('n', "<leader>ff", "<cmd>Telescope find_files<cr>")
+key_mapper('n', "<leader>fg", "<cmd>Telescope live_grep<cr>")
 key_mapper('n', "<leader>fb", "<cmd>Telescope buffers<cr>")
-key_mapper('n', "<leader>fh", " <cmd>Telescope help_tags<cr>")
+key_mapper('n', "<leader>fh", "<cmd>Telescope help_tags<cr>")
