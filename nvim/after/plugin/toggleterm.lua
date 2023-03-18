94866cbbba450f70dc8fd7af2f28cf0ff24ac217
Cristian Webber <cristian.webber@hotmail.com>
2023-03-18 11:39:35 -0300

    feat: add float terminal

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,24 +1,29 @@
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({
-    cmd = "lazygit",
-    dir = "git_dir",
+require('toggleterm').setup {
+    active = true,
+    on_config_done = nil,
+    size = 20,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 2,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = false,
     direction = "float",
+    close_on_exit = true,
+    shell = nil,
     float_opts = {
-        border = "double",
+        border = "curved",
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        },
     },
-    -- function to run on opening the terminal
-    on_open = function(term)
-        vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
-    end,
-    -- function to run on closing the terminal
-    on_close = function(term)
-        vim.cmd("startinsert!")
-    end,
-})
-
-function _lazygit_toggle()
-    lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+    execs = {
+        { nil, "<M-1>", "Horizontal Terminal", "horizontal", 0.3 },
+        { nil, "<M-2>", "Vertical Terminal",   "vertical",   0.4 },
+        { nil, "<M-3>", "Float Terminal",      "float",      nil },
+    },
+}
